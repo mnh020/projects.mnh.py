a61a2fabe5abedbe4ebc8b94c7e9bd3b252eb0fd
MNH <167299907+mnh020@users.noreply.github.com>
2024-05-22 03:36:52 +0200

    Add files via upload
    Exercise 1:
    
    We use variables for name, age, and hobbies and incorporate them into a formatted string using an f-string.

---
+++ #1 Variables for name, age, and hobbies.py
@@ -0,0 +1,10 @@
+# Variables for name, age, and hobbies
+name = "Michelle Horvat"
+age = 38
+hobbies = "learning Python"
+
+# Using an f-string to incorporate the variables into a formatted string
+introduction = f"My name is {name}, I am {age} years old. I enjoy {hobbies}. I am originally from a small town. I used to work in different fields and am now dedicated to mastering Python programming."
+
+# Print the introduction
+print(introduction)
