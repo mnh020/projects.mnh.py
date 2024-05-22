ba62d7414ea7082b7b04724a9b86d257aba964ba
MNH <167299907+mnh020@users.noreply.github.com>
2024-05-22 03:20:55 +0200

    Add files via upload
    Exercise 1:
    
    We use triple quotes """ to print the multi-line string exactly as specified.

---
+++ Exercise 1.py
@@ -0,0 +1,7 @@
+# Exercise 1: Print the specified string in the given format
+print("""Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+Twinkle, twinkle, little star,
+How I wonder what you are.""")
