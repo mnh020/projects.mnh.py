7ef68e17b509269494a0e8da64a21ce113c45893
MNH <167299907+mnh020@users.noreply.github.com>
2024-10-30 10:14:47 +0100

    1 Variables for name, age, and hobbies.py

--- #1 Variables for name, age, and hobbies.py
+++ #1 Variables for name, age, and hobbies.py
@@ -1,12 +1,12 @@
-# Variables for name, age, and hobbies
+Variables for name, age, and hobbies
 name = "Michelle Horvat"
 age = 38
 hobbies = "learning Python"
 
-# Using an f-string to incorporate the variables into a formatted string
+Using an f-string to incorporate the variables into a formatted string
 introduction = f"My name is {name}, I am {age} years old. I enjoy {hobbies}. I am originally from a small town. I used to work in different fields and am now dedicated to mastering Python programming."
 
-# Print the introduction
+Print the introduction
 print(introduction)
 
 My name is Michelle Horvat, I am 38 years old. I enjoy learning Python. I am originally from a small town. I used to work in different fields and am now dedicated to mastering Python programming.
